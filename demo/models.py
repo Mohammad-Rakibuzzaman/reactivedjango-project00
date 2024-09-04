2d6839a6528c8449110c42de870c4673e2c3c7fd
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-09-04 18:14:51 +0600

    feat: aded doc and urls.py both project and app

--- demo/models.py
+++ demo/models.py
@@ -13,3 +13,19 @@ class Book(models.Model):
     #                          default='', choices=STATUSES)
 
     title = models.CharField(max_length=36, blank=False, unique=True)
+    description = models.TextField(max_length=256, blank=True)
+
+    # price = models.FloatField(default=0, max_digits=0, decimal_places=2)
+    price = models.DecimalField(default=0, max_digits=4, decimal_places=2)
+    # price = models.BigIntegerField(default=0)
+
+    # published = models.DateField(auto_now=True, auto_now_add=True)
+    # published = models.TimeField(auto_now=True, auto_now_add=True)
+    # published = models.DateTimeField(auto_now=True, auto_now_add=True)
+    published = models.DateField(blank=True, null=True, default=None)
+    is_published = models.BooleanField(default=False)
+
+    # cover = models.FileField(upload_to='covers')
+    cover = models.ImageField(upload_to='covers', blank=True)
+
+
