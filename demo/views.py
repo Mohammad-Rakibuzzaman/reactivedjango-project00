b0193a6890fa7c9e5170f4385c6e9d50c51e86c3
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-09-04 20:24:39 +0600

    feat:practice classbased view

--- demo/views.py
+++ demo/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+
 # Create your views here.
+
+class Another(View):
+    def get(self, request):
+        return HttpResponse('another method in class!')
+
+
 def first(request):
     return HttpResponse('First message!')
