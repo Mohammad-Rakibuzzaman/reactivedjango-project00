ca35b079487f5d13d250a3b582f62dc01a298c92
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-09-04 10:39:23 +0600

    feat: Model options added

--- demo/models.py
+++ demo/models.py
@@ -2,4 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=36)
+
+    # STATUSES = (
+    #     (0, 'Unknown'),
+    #     (1, 'processed'),
+    #     (2, 'paid')
+    # )
+
+    # title = models.CharField(null=True, blank=False, unique=True,
+    #                          default='', choices=STATUSES)
+
+    title = models.CharField(max_length=36, blank=False, unique=True)
